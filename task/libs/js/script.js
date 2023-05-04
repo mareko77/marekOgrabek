0fb151fd1610f6120e0c23b591ca44e78d25de03
mareko77 <36967486+mareko77@users.noreply.github.com>
2023-05-04 20:23:53 +0100

    Added working API's

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -1,32 +1,76 @@
-	$('#btnRun1').click(function() {
+$('#btnRun1').click(function() {
+    $.ajax({
+        url: "libs/php/ocean.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            lat: $('#selLatitude').val(),
+            lng: $('#selLongitude').val()
+        },
+        success: function(result) {
+            console.log(JSON.stringify(result));
+            console.log(JSON.stringify(result.data));
+           
+            if (result.status.name == "ok") {
+                $('#result1').html(result['data']['name']);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('#result1').html("There isn't any ocean or sea here!");
+        }
+    }); 
+});
 
-		$.ajax({
-			url: "libs/php/threeApiApps.php",
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				latitude: $('#lat').val(),
-				longitude: $('#lng').val()
-			},
-			success: function(result) {
+$('#btnRun2').click(function() {
+    $.ajax({
+        url: "libs/php/weather.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            lat1: $('#selLatitude1').val(),
+            lng1: $('#selLongitude1').val()
+        },
+        success: function(result) {
+            console.log(JSON.stringify(result));
+            console.log(JSON.stringify(result.data));
+           
+            if (result.status.name == "ok") {
+                $('#stationNameResult').html(result['data']['stationName']);
+                $('#countryCodeResult').html(result['data']['countryCode']);
+                $('#temperatureResult').html(result['data']['temperature']);
+                $('#humidityResult').html(result['data']['humidity']);
+                $('#cloudsResult').html(result['data']['clouds']);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('#stationNameResult').html("There is no data for this location!");
+        }
+    }); 
+});
 
-				console.log(JSON.stringify(result));
+$('#btnRun3').click(function() {
+    $.ajax({
+        url: "libs/php/currency.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            currency: $('#currency').val(),
+            lang: $('#language').val()
+        },
+        success: function(result) {
+            console.log(JSON.stringify(result));
+            console.log(JSON.stringify(result.data));
 
-				if (result.status.name == "ok") {
 
-					$('#distance').html(result['data'][0]['distance']);
-					$('#geonameId').html(result['data'][0]['geonameId']);
-					$('#name').html(result['data'][0]['name']);
-
-				}
-			
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				// your error code
-			}
-		}); 
-	
-	});
+            if (result.status.name == "ok") {
+                $('#currencyResult').html(result['data'][0]['currencyCode']);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('#currencyResult').html("There is an error! Try again!");
+        }
+    }); 
+});
 
 
 	
