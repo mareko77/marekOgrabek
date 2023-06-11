f521cf01187fb8244ff8b6bffab671ca14e86337
mareko77 <36967486+mareko77@users.noreply.github.com>
2023-06-11 21:03:06 +0100

    Add files via upload

--- project1/gazetteer/libs/js/main.js
+++ project1/gazetteer/libs/js/main.js
@@ -51,6 +51,12 @@ const weatherInfoButton = new L.easyButton('<i class="fas fa-cloud-sun"></i>', f
 }, "Weather Info");
 weatherInfoButton.addTo(map);
 
+// button to show Modal with Country flag
+const countryFlagButton = new L.easyButton('<i class="fa fa-flag"></i>', function() {
+    $("#flagModal").modal("show");
+}, "Country Flag");
+countryFlagButton.addTo(map);
+
 // Get User Location
 function userLocation() {
     if (!navigator.geolocation) {
@@ -58,16 +64,14 @@ function userLocation() {
     }
 
     function success(position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
 
         $.ajax({
             url: "libs/php/getUserIsoA2.php",
             type: 'POST',
             dataType: 'json',
             data: {
-                lat: latitude,
-                lng: longitude
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
             },
             success: function(result) {
                 $("#select-country").val(result.isoA2).change();
@@ -134,8 +138,8 @@ function addBorders(country) {
                 map.removeLayer(border);
             }
            border = L.geoJSON(countryBorders, {
-               color: "#361999",
-               wheight: 14,
+               color: blue,
+               weight: 14,
                opacity: 1,
                fillOpacity: 0.1
            }).addTo(map);
@@ -199,8 +203,6 @@ $("#select-country").change(function(){
 });
 
 //Weather
-let capitalLat;
-let capitalLng;
 
 $("#select-country").change(function(){
     $.ajax({
@@ -208,17 +210,8 @@ $("#select-country").change(function(){
         type: 'POST',
         dataType: 'json',
         data: {
-            country: $('#select-country option:selected').text()
+            country: encodeURI($('#select-country option:selected').text())
         }, 
-/*$("#select-country").change(function(){
-        $.ajax({
-            url: "libs/php/getUserIsoA2.php",
-            type: 'POST',
-            dataType: 'json',
-            data: {
-                lat: latitude,
-                lng: longitude
-            },*/
         success: function(result) {
             console.log(result);
             if (result.status.name == "ok") {
@@ -227,8 +220,8 @@ $("#select-country").change(function(){
                     type: 'GET',
                     dataType: "json",
                     data: {
-                        capitalLat: result.latitude,
-                        capitalLng: result.longitude
+                        capitalLat: result.data[0].geometry.lat,
+                        capitalLng: result.data[0].geometry.lng
             
                         
                     },
@@ -238,27 +231,37 @@ $("#select-country").change(function(){
                     success: function(result1) {
                         console.log(result1);
                         $("#weatherTable tbody tr td").html("&nbsp;"); 
-                        let weatherIcon = result1.data.weather.current.weather[0].icon;
-                            
-                        $('.txtCapitalWeatherName').html(capitalCityName);
+                        let weatherIconCurrent = result1.data.weather.current.weather[0].icon;
+
+                        $('#capitalWeatherIcon').html( `<img src="https://openweathermap.org/img/wn/${weatherIconCurrent}@2x.png" width="120px">`);
+                        $('#txtCapitalWeatherName').html(result1.data.weather.timezone);
                         $('#txtCapitalWeatherCurrent').html( Math.round(result1.data.weather.current.temp) +'&#8451<br>');
                         $('#txtCapitalWeatherDescription').html( result1.data.weather.current.weather[0].description);
                         $('#txtCapitalWeatherWindspeed').html(result1.data.weather.current.wind_speed + ' km/h');
                         $('#txtCapitalWeatherHumidity').html( Math.round(result1.data.weather.current.humidity) +'&#37');
                         $('#txtCapitalWeatherLo').html( Math.round(result1.data.weather.daily[0].temp.min) +'&#8451<br>');
                         $('#txtCapitalWeatherHi').html( Math.round(result1.data.weather.daily[0].temp.max) +'&#8451<br>');
+
+                        var sunrise = moment( result1.data.weather.current.sunrise*1000).format("HH:mm");
+                        $('#txtCapitalSunrise').html(sunrise);
+                        var sunset = moment( result1.data.weather.current.sunset*1000).format("HH:mm");
+                        $('#txtCapitalSunset').html(sunset);
+                        $('.capitalSunriseIcon').html('<img src="images/icons/sunrise.png" width="24px">');
+                        $('.capitalSunsetIcon').html('<img src="images/icons/sunset.png" width="24px">');
+
                         $('#txtCapitalTomorrowsWeatherLo').html( Math.round(result1.data.weather.daily[1].temp.min) +'&#8451<br>');
                         $('#txtCapitalTomorrowsWeatherHi').html( Math.round(result1.data.weather.daily[1].temp.max) +'&#8451<br>');
-                        $('#CapitalWeatherIcon').html( `<img src="https://openweathermap.org/img/wn/${weatherIcon}@2x.png" width="24px">`);
-                        $('#CapitalHumidityIcon').html('<img src="assets/img/icons/humidity.svg" width="24px">');
-                        $('#CapitalWindIcon').html('<img src="assets/img/icons/007-windy.svg" width="24px">');
-                        $('.CapitalHiTempIcon').html('<img src="assets/img/icons/temperatureHi.svg" width="24px">');
-                        $('.CapitalLoTempIcon').html('<img src="assets/img/icons/temperatureLo.svg" width="24px">');
-            
                        
-                       
+                        $('#weatherIcon').html('<img src="images/icons/weather.svg" width="24px">');
+                        $('#capitalHumidityIcon').html('<img src="images/icons/humidity.svg" width="24px">');
+                        $('#capitalWindIcon').html('<img src="images/icons/007-windy.svg" width="24px">');
+                        $('.capitalHiTempIcon').html('<img src="images/icons/thermometer.svg" width="24px">');
+                        $('.capitalLoTempIcon').html('<img src="images/icons/thermometer-colder.svg" width="24px">');
+
+                        
+                        
             
-                        $('#weatherModal').modal('show');
+                        //$('#weatherModal').modal('show');
             
                     },
                     complete: function () {
@@ -267,20 +270,107 @@ $("#select-country").change(function(){
                     error: function(jqXHR,textStatus, errorThrown) {
                         console.error(jqXHR);
                     }
-                });
-                    
-            }
-                
-           
-        },
-        error: function(jqXHR,textStatus, errorThrown) {
-            console.error(jqXHR);
+                });        
+            } 
         }
-    });  
-    
-    
+    });    
 });
 
+//Flag
+$("#select-country").change(function() {
+    $.ajax({
+        url: "libs/php/getFlag.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            code: $('#select-country option:selected').val()
+        },
+        beforeSend: function () {
+            $("#loader").removeClass("hidden");
+        },
+        success: function(result) {
+
+            let countryFlag = $("#country-flag");
+            countryFlag.html("");
+
+                countryFlag.append($(
+                    `<div class="card h-100 country">
+                            <img src="${result.flag}" alt="${result.name}"/>
+                    </div>
+                    <div class="card-body country">
+                        <h4><a href="https://en.wikipedia.org/wiki/${result.name}" target="_blank">${result.name}</a></h4>
+                    </div>`
+                ));
+                $("#countryFlagModal").modal("show");
+                
+        },
+        complete: function () {
+            $("#loader").addClass("hidden")
+        },
+        error: function(textStatus, errorThrown) {
+            alert("Error: " + errorThrown);
+        }
+    });    
+});
+
+
+
+//earthquake 
+
+let earthquake = L.markerClusterGroup();
+map.addLayer(earthquake);
+
+const earthquakeIcon = L.icon({
+    iconUrl: 'images/earthquakeIcon.png',
+    iconSize: [50, 50],
+    iconAnchor: [25, 45],
+    popupAnchor: [0, -40]
+});
+
+function getEarthquakes(north, south, east, west) {
+    earthquakesCG.clearLayers();
+    $.ajax({
+        url: "libs/php/getEarthquake.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            north: north,
+            south: south,
+            east: east,
+            west: west
+        },
+        success: function(result) {    
+            const earthquakes = result.data;
+            
+            earthquakes.forEach(earthquake => {
+                const earthquakeMarker = L.marker([`${earthquake.lat}`, `${earthquake.lng}`], {icon: earthquakeIcon})
+                    .bindPopup(`
+                            <div class="container card h-100">
+                                <table class="table table-sm">
+                                    <thead>
+                                        <tr>
+                                            <th><strong>Earthquake</strong></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">Magnitude:</th> <td class="text-end"><strong>${earthquake.magnitude}</strong></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Date and Time:</th> <td class="text-end">${window.moment(earthquake.datetime).format('MMMM Do YYYY, h:mm:ss A')}<td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            <div>`
+                );
+                earthquakesCG.addLayer(earthquakeMarker);
+            });
+        },
+        error: function(textStatus, errorThrown) {
+            // console.log(textStatus, errorThrown);
+        }
+    });    
+}
 
 
 
